e211fb7fb983be562f45886ff012fdc9496a4adc
Ritam Bhowmick <iammritam11@gmail.com>
2025-07-26 02:21:21 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spliter",
+  description: "The smartest way to split expenses with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body
+        className={`${inter.className}`}
+      >
+      <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Staring New Project</div>
+  );
+}
